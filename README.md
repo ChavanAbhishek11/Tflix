b74523093047014329347c06cb4b9ee979cc7326
Abhishek pandurang chavan <chavanabhishek724@gmail.com>
2023-06-03 06:05:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+API_URL="https://api.themoviedb.org/3/movie/popular?api_key=9962250b5b92e693f0fb7b0cd065e8cb"
+API_IMG="https://image.tmdb.org/t/p/w500"
+API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=9962250b5b92e693f0fb7b0cd065e8cb&query"
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Component/Main';
+import Product from './Component/Product';
+
+function App() {
+  return (
+    <>
+    <Main/>
+    <Product/>
+    </>
+  );
+}
+
+export default App;
